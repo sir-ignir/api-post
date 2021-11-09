c23a4acd56f6fa6c375a353f829f154b3cb55923
sir-ignir <magniez.maxence45@gmail.com>
2021-11-09 15:07:56 +0100

    ora

--- src/api/Post.js
+++ src/api/Post.js
@@ -3,14 +3,16 @@ import axios from "axios";
 import PostCard from "./PostCard";
 import "./Post.css";
 function Post() {
+  const [isLoading, setIsLoading] = useState(true);
   const [Post, setPost] = useState([]);
   const [Users, setUsers] = useState([]);
   const fetchData = async () => {
+    setIsLoading(true);
     try {
       const url = await axios.get("https://jsonplaceholder.typicode.com/posts");
       const aut = await axios.get("https://jsonplaceholder.typicode.com/users");
       setUsers(aut.data);
-
+      setTimeout(() => setIsLoading(false), 800);
       setPost(url.data);
     } catch (error) {
       console.log("error =>", error);
@@ -22,17 +24,21 @@ function Post() {
 
   return (
     <div>
-      <div id="CardPost">
-        {Post.map((post) => {
-          return (
-            <PostCard
-              title={post.title}
-              body={post.body}
-              name={Users.find((i) => i.id == post.userId)}
-            />
-          );
-        })}
-      </div>
+      {isLoading ? (
+        <p>getting data</p>
+      ) : (
+        <div id="CardPost">
+          {Post.map((post) => {
+            return (
+              <PostCard
+                title={post.title}
+                body={post.body}
+                name={Users.find((i) => i.id == post.userId)}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
--- src/api/PostCard.js
+++ src/api/PostCard.js
@@ -1,9 +1,10 @@
 const PostCard = ({ title, body, name }) => {
   return (
     <div className="postCardContainer">
+      <img src="https://source.unsplash.com/random/200x100" />
       <h3>{title}</h3>
       <p>{body}</p>
-      <p style={{ color: "yellow" }}>{name}</p>
+      <p style={{ color: "red" }}>{name.name}</p>
     </div>
   );
 };
